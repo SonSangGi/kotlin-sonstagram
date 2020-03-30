90fcc708b93f9d680952d930fd9d07325b6751f5
sgson <ssg3799@gmail.com>
2020-03-30 15:41:40 +0900

    Feat: 네비게이션바 추가

--- app/src/main/java/dev/sanggi/sonstagram/MainActivity.kt
+++ app/src/main/java/dev/sanggi/sonstagram/MainActivity.kt
@@ -2,11 +2,49 @@ package dev.sanggi.sonstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import dev.sanggi.sonstagram.navigation.AlarmFragment
+import dev.sanggi.sonstagram.navigation.DetailViewFragment
+import dev.sanggi.sonstagram.navigation.GridFragment
+import dev.sanggi.sonstagram.navigation.UserFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_home -> {
+                var detailViewFragment = DetailViewFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, detailViewFragment).commit()
+                return true
+            }
+            R.id.action_search -> {
+                var gridFragment = GridFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, gridFragment).commit()
+                return true
+            }
+            R.id.action_photo -> {
+
+                return true
+            }
+            R.id.action_favorite_alarm -> {
+                var alarmFragment = AlarmFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, alarmFragment).commit()
+                return true
+            }
+            R.id.action_account -> {
+                var userFragment = UserFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
+                return true
+            }
+        }
+        return false
     }
 }
