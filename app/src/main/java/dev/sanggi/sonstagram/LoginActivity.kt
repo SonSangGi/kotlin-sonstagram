9f7d2ed17dbddd144d28b41573c2504a7eca2c4d
sgson <ssg3799@gmail.com>
2020-03-31 15:44:04 +0900

    Feat: 아이디저장, 자동 로그인 추가

--- app/src/main/java/dev/sanggi/sonstagram/LoginActivity.kt
+++ app/src/main/java/dev/sanggi/sonstagram/LoginActivity.kt
@@ -18,6 +18,9 @@ import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    val TAG = LoginActivity::class.simpleName
+
     var auth: FirebaseAuth? = null
     var googleSignInClient: GoogleSignInClient? = null
     var GOOGLE_LOGIN_CODE = 9001
@@ -26,13 +29,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        autoLoginCheck()
-
         auth = FirebaseAuth.getInstance()
 
+        autoLoginCheck()
+
         email_login_button.setOnClickListener {
-            signinAndSignUp()
-            autoLoginSave()
+            if (email_edittext.text.toString().isNullOrEmpty() || password_edittext.text.toString().isNullOrEmpty())
+                Toast.makeText(this, "아이디 혹은 비밀번호를 입력해주세요.", Toast.LENGTH_LONG).show()
+            else {
+                signinAndSignUp()
+            }
         }
         google_login_button.setOnClickListener {
             googleLogin()
@@ -47,14 +53,93 @@ class LoginActivity : AppCompatActivity() {
 
     }
 
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                var account = result.signInAccount
+                // Second step
+                firebaseAuthWithGoogle(account)
+
+            }
+        }
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // 로그인
+                    moveMainPage(task.result?.user)
+                } else {
+                    // 에러 메세지
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    Log.e(TAG, task.exception?.message)
+                }
+            }
+
+    }
+
+    fun signinAndSignUp() {
+        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // 유저 생성
+                    Toast.makeText(this, "아이디가 생성되었습니다.", Toast.LENGTH_LONG).show()
+                    moveMainPage(task.result?.user)
+                } else if (task.exception?.message.isNullOrEmpty()) {
+                    // 오류 메세지
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                    Log.e(TAG, task.exception?.message)
+                } else {
+                    // 로그인
+                    signinEmail()
+                }
+            }
+    }
+
+    fun signinEmail() {
+        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // 로그인
+                    moveMainPage(task.result?.user)
+                } else {
+                    // 에러 메세지
+                    Toast.makeText(this, "아이디 혹은 비밀번호가 일치하지 않습니다.", Toast.LENGTH_LONG).show()
+                    Log.w(TAG, task.exception?.message)
+                }
+            }
+    }
+
+    fun moveMainPage(user: FirebaseUser?) {
+        if (user != null) {
+            autoLoginSave()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+
     fun autoLoginCheck() {
         val auto = getSharedPreferences("auto", MODE_PRIVATE)
 
         val autoLogin: Boolean = auto.getBoolean("auto_login", false)
         val saveId: Boolean = auto.getBoolean("save_id", false)
 
-        if (autoLogin)
-            Toast.makeText(this, "자동로그인 체크 됨", Toast.LENGTH_SHORT)
+        saveid_checkbox.isChecked = saveId
+        autologin_checkbox.isChecked = autoLogin
+
+        if (autoLogin) {
+            email_edittext.setText(auto.getString("email", ""))
+            password_edittext.setText(auto.getString("password", ""))
+            signinEmail()
+        }
 
         if (saveId) {
             saveid_checkbox.isChecked = saveId
@@ -72,76 +157,9 @@ class LoginActivity : AppCompatActivity() {
         if (saveid_checkbox.isChecked)
             edit.putString("email", email_edittext.text.toString())
 
+        if (autologin_checkbox.isChecked)
+            edit.putString("password", password_edittext.text.toString())
+
         edit.commit()
-    }
-
-    fun googleLogin() {
-        var signInIntent = googleSignInClient?.signInIntent
-        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == GOOGLE_LOGIN_CODE){
-            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if(result.isSuccess) {
-                var account = result.signInAccount
-                // Second step
-                firebaseAuthWithGoogle(account)
-
-            }
-        }
-    }
-
-    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
-        var credential =  GoogleAuthProvider.getCredential(account?.idToken, null)
-        auth?.signInWithCredential(credential)
-            ?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // 로그인
-                    moveMainPage(task.result?.user)
-                } else {
-                    // 에러 메세지
-                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    Log.e(LoginActivity::class.simpleName, task.exception?.message)
-                }
-            }
-
-    }
-
-    fun signinAndSignUp() {
-        auth?.createUserWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
-            ?.addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    // 유저 생성
-                    moveMainPage(task.result?.user)
-                } else if (task.exception?.message.isNullOrEmpty()) {
-                    // 오류 메세지
-                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    Log.e(LoginActivity::class.simpleName, task.exception?.message)
-                } else {
-                    // 로그인
-                    signinEmail()
-                }
-            }
-    }
-
-    fun signinEmail() {
-        auth?.signInWithEmailAndPassword(email_edittext.text.toString(), password_edittext.text.toString())
-            ?.addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            // 로그인
-                            moveMainPage(task.result?.user)
-                } else {
-                    // 에러 메세지
-                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
-                    Log.e(LoginActivity::class.simpleName, task.exception?.message)
-                }
-            }
-    }
-
-    fun moveMainPage(user: FirebaseUser?) {
-        if (user != null)
-            startActivity(Intent(this, MainActivity::class.java))
     }
 }
